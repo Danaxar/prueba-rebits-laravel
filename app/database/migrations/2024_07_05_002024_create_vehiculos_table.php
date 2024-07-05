d06c4c5b9b0cd5faf92e09dd74e3486ace87526a
Danaxar <daniel.catalan@usach.cl>
2024-07-04 21:54:38 -0400

    feat: migraciones para usuarios, vehiculos e historicos

---
+++ app/database/migrations/2024_07_05_002024_create_vehiculos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vehiculos', function (Blueprint $table) {
+            $table->id();
+            $table->string('marca');
+            $table->string('modelo');
+            $table->string('patente')->unique();
+            $table->integer('anio');
+            $table->integer('precio');
+            $table->timestamps();
+
+            // Llaves foráneas
+            $table->integer('id_usuario');
+            $table->foreign('id_usuario')->references('id')->on('usuarios');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vehiculos');
+    }
+};
