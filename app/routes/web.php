9da583bd225b318dba05935aac0da2bb5f67e42b
Danaxar <daniel.catalan@usach.cl>
2024-07-06 13:18:41 -0400

    feat: rutas

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,7 +1,39 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsuariosController;
+use App\Http\Controllers\VehiculoController;
+use App\Http\Controllers\ExcelController;
+use Inertia\Inertia;
 
+// API
+Route::prefix('api')->group(function () {
+    // Usuarios
+    Route::prefix('usuarios')->group(function () {
+        Route::get('/', [UsuariosController::class, 'getAll']);
+    });
+
+    // Vehiculos
+    Route::prefix('vehiculos')->group(function () {
+        Route::get('/', [VehiculoController::class, 'getAll']);
+    });
+
+    // Excel
+    // Route::prefix('excel')->group(function () {
+    //     Route::post('/upload', [ExcelController::class, 'upload']);
+    // });
+});
+
+// Inertia
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Home');
+});
+Route::get('/vehiculos', function () {
+    return Inertia::render('VehiculosPage');
+});
+Route::get('/usuarios', function () {
+    return Inertia::render('UsuariosPage');
+});
+Route::get('/historicos', function () {
+    return Inertia::render('HistoricosPage');
 });
