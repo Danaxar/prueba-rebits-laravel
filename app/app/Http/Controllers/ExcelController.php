ca439fc07082cb015a71c787cb54927bf735729d
Danaxar <daniel.catalan@usach.cl>
2024-07-08 20:02:27 -0400

    feat: notificación por correo electronico

--- app/app/Http/Controllers/ExcelController.php
+++ app/app/Http/Controllers/ExcelController.php
@@ -7,6 +7,8 @@ use App\Imports\ExcelImport;
 use Maatwebsite\Excel\Facades\Excel;
 // use Excel; // Otra forma de importar la clase
 use Symfony\Component\Console\Output\ConsoleOutput;
+use App\Mail\ExcelImportNotification;
+use Illuminate\Support\Facades\Mail;
 
 class ExcelController extends Controller
 {
@@ -23,11 +25,24 @@ class ExcelController extends Controller
         $import = new ExcelImport();
         Excel::import($import, $request->file('file'));
 
+        $output->writeln('Enviando correo de notificación...');
+        $mensajeCorreo = '';
+        if (count($import->getMessages()) == 0) {
+            $mensajeCorreo = '¡El archivo excel se ha procesado exitosamente!';
+        } else {
+            $mensajeCorreo = 'El archivo excel se ha procesado con alertas.';
+        }
+        try {
+            // Mail::to('daniel.catalan@usach.cl')->send(new ExcelImportNotification($mensajeCorreo));
+            Mail::to(env('EMAIL_TO'))->send(new ExcelImportNotification($mensajeCorreo));
+        } catch (\Exception $e) {
+            $output->writeln('Error al enviar el correo: ' . $e->getMessage());
+        }
         // Obtener los mensajes post importación
         return response()->json([
             'messages' => $import->getMessages(),
             'status' => 'success',
             'rows' => $import->getLines()
-        ]);
+        ], 200);
     }
 }
